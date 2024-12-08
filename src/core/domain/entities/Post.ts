936e8bb8e48233021f50051daffbd7a26e020c27
John O’Sullivan <osullivanj.01@gmail.com>
2024-12-08 12:19:08 +0000

    Step 5: Red Phase - Write a Failing Test for CreatePost Use Case

--- src/core/domain/entities/Post.ts
+++ src/core/domain/entities/Post.ts
@@ -1,21 +1,35 @@
+import { IdGenerator } from "../interfaces/IdGenerator";
+
 export class Post {
-  id: string;
-  title: string;
-  content: string;
-  author: string;
-  createdAt: Date;
-  updatedAt: Date;
+  #id;
+  #title;
+  #content;
+  #author;
+  #createdAt;
+  #updatedAt;
 
-  constructor(title: string, content: string, author: string) {
-    this.id = generateId();
-    this.title = title;
-    this.content = content;
-    this.author = author;
-    this.createdAt = new Date();
-    this.updatedAt = new Date();
+  constructor(
+    title: string,
+    content: string,
+    author: string,
+    idGenerator: IdGenerator,
+  ) {
+    this.#id = idGenerator.generateId();
+    this.#title = title;
+    this.#content = content;
+    this.#author = author;
+    this.#createdAt = new Date();
+    this.#updatedAt = new Date();
   }
-}
 
-function generateId(): string {
-  return Math.random().toString(36) // Simple ID value for now
+  toObject() {
+    return {
+      id: this.#id,
+      title: this.#title,
+      content: this.#content,
+      author: this.#author,
+      createdAt: this.#createdAt,
+      updatedAt: this.#updatedAt,
+    };
+  }
 }
--- src/tests/core/domain/entities/Post.spec.ts
+++ src/tests/core/domain/entities/Post.spec.ts
@@ -1,12 +1,30 @@
-import { Post } from '../../../../../src/core/domain/entities/Post';
+import { Post } from "../../../../../src/core/domain/entities/Post";
+import { IdGenerator } from "../../../../../src/core/domain/interfaces/IdGenerator";
 
-describe('Post', () => {
-  it('should create a new Post with title, content, and author', () => {
-    const post = new Post('My First Post', 'This is the content.', 'John Doe');
-    expect(post.title).toBe('My First Post');
-    expect(post.content).toBe('This is the content.');
-    expect(post.author).toBe('John Doe');
-    expect(post.createdAt).toBeDefined();
-    expect(post.updatedAt).toBeDefined();
+class MockIdGenerator implements IdGenerator {
+  generateId() {
+    return "test-id";
+  }
+}
+
+describe("Post", () => {
+  it("should create a new Post with title, content, and author", () => {
+    const title = "My First Post";
+    const content = "This is the content.";
+    const author = "John Doe";
+
+    const mockIdGenerator = new MockIdGenerator();
+    const post = new Post(title, content, author, mockIdGenerator);
+
+    const expected = {
+      id: expect.any(String),
+      title: title,
+      content: content,
+      author: author,
+      createdAt: expect.any(Date),
+      updatedAt: expect.any(Date),
+    };
+
+    expect(post.toObject()).toStrictEqual(expected);
   });
 });
